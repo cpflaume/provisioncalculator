9546f779f63ffdd99e27c930c652cd28e7eb19de
cpflaume <constantin.pflaume@gmail.com>
2026-04-26 08:24:08 +0200

    refactor: add @EnableScheduling for rate-limit window eviction

--- src/main/kotlin/com/provisions/calculator/ProvisionCalculatorApplication.kt
+++ src/main/kotlin/com/provisions/calculator/ProvisionCalculatorApplication.kt
@@ -2,8 +2,10 @@ package com.provisions.calculator
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class ProvisionCalculatorApplication
 
 fun main(args: Array<String>) {
